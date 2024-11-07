2d39d7126e88b653231954aabb8ade6402bab58e
Eijnarb <branjiebacussanchez@gmail.com>
2024-11-08 06:27:33 +0800

    one email only

--- ASI.Basecode.Services/Interfaces/IUserService.cs
+++ ASI.Basecode.Services/Interfaces/IUserService.cs
@@ -2,11 +2,10 @@
 using ASI.Basecode.Services.ServiceModels;
 using static ASI.Basecode.Resources.Constants.Enums;
 
-namespace ASI.Basecode.Services.Interfaces
+public interface IUserService
 {
-    public interface IUserService
-    {
-        LoginResult AuthenticateUser(string userIdOrEmail, string password, ref User user);
-        void AddUser(UserViewModel model);
-    }
+    LoginResult AuthenticateUser(string userIdOrEmail, string password, ref User user);
+    void AddUser(UserViewModel model);
+    bool CheckEmailExists(string email);
 }
+
