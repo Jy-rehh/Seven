65fba32b9c241fea307573715ec838140d9b23c5
Eijnarb <branjiebacussanchez@gmail.com>
2024-11-10 12:27:01 +0800

    a

--- ASI.Basecode.Data/Models/Settings.cs
+++ ASI.Basecode.Data/Models/Settings.cs
@@ -1,11 +1,18 @@
-﻿namespace ASI.Basecode.Data.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;  // Required for [Key] attribute
+
+namespace ASI.Basecode.Data.Models
 {
     public class Settings
     {
-        public string Username { get; set; }
-        public string Field1 { get; set; }  // Example field
-        public string Field2 { get; set; }  // Example field
+        [Key]  // Marking this property as the primary key
+        public int SettingsId { get; set; }  // Primary key property
+
+        public string Username { get; set; }  // Example field for username
+        public string Field1 { get; set; }  // Example field 1
+        public string Field2 { get; set; }  // Example field 2
     }
 }
+
 
 
