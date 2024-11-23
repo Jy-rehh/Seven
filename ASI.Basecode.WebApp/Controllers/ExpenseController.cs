f2e8e26931cd800e36f8777f052c5ca52e3adc6f
larahlaneserdan <143809565+larleynn@users.noreply.github.com>
2024-11-23 11:22:09 +0800

    Expense and Report
    Expense Controller
    Report Index
    Expense Index

--- ASI.Basecode.WebApp/Controllers/ExpenseController.cs
+++ ASI.Basecode.WebApp/Controllers/ExpenseController.cs
@@ -1,108 +1,76 @@
-﻿using ASI.Basecode.Services.Interfaces;
+﻿using ASI.Basecode.Data.Models;
+using ASI.Basecode.Services.Interfaces;
 using ASI.Basecode.Services.ServiceModels;
-using ASI.Basecode.Services.Services;
 using ASI.Basecode.WebApp.Mvc;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace ASI.Basecode.WebApp.Controllers
+public class ExpenseController : ControllerBase<ExpenseController>
 {
-    public class ExpenseController : ControllerBase<ExpenseController>
+    private readonly IExpenseService _expensesService;
+
+    public ExpenseController(
+        IHttpContextAccessor httpContextAccessor,
+    ILoggerFactory loggerFactory,
+        IConfiguration configuration,
+        IExpenseService expensesService,
+        IMapper mapper = null) : base(httpContextAccessor, loggerFactory, configuration, mapper)
     {
-        private readonly IExpenseService _expensesService;
-        public ExpenseController(
-                                IHttpContextAccessor httpContextAccessor,
-                                ILoggerFactory loggerFactory,
-                                IConfiguration configuration,
-                                IExpenseService expensesService,
-                                IMapper mapper = null) : base(httpContextAccessor, loggerFactory, configuration, mapper)
-        {
-            _expensesService = expensesService;
-        }
-        public IActionResult Index()
-        {
-            var expenses = _expensesService.GetExpenseByUserId(UserId);
-            var categories = _expensesService.GetCategories();
-            var data = new ExpenseDataModel
-            {
-                ExpenseViewModel = expenses,
-                CategoryViewModel = categories
-            };
-            //var data = _expensesService.GetAllExpenses();
-            return View(data);
-        }
-
-        #region Get Methods
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpGet]
-        public IActionResult Edit(int Id)
-        {
-            var data = _expensesService.RetrieveExpenses(Id);
-            return View(data);
-        }
-        [HttpGet]
-        public IActionResult Delete(int Id)
-        {
-            var data = _expensesService.RetrieveExpenses(Id);
-            return View(data);
-        }
-        #endregion
-        //public IActionResult Expenses(List<string> selectedCategories)
-        //{
-        //    var expenses = _expensesService.GetAllExpenses();
-
-        //    if (selectedCategories != null && selectedCategories.Any())
-        //    {
-        //        // Attempt to convert selectedCategories to a list of integers
-        //        var selectedCategoryIds = selectedCategories
-        //            .Where(c => int.TryParse(c, out _))
-        //            .Select(int.Parse)
-        //            .ToList();
-
-        //        // Filter expenses by integer CategoryId
-        //        expenses = expenses.Where(e => selectedCategoryIds.Contains(e.CategoryId)).ToList();
-        //    }
-
-        //    return View(expenses);
-        //}
-
-
-        #region Post Methods
-
-        [HttpPost]
-        public IActionResult Create(ExpenseViewModel model)
-        {
-            TempData["SuccessMessage"] = "Expense added successfully.";
-            _expensesService.AddExpenses(model, UserId);
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        public IActionResult Edit(ExpenseViewModel model)
-        {
-            TempData["SuccessMessage"] = "Expense updated successfully.";
-            _expensesService.UpdateExpenses(model, UserId);
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        public IActionResult PostDelete(int ExpenseId)
-        {
-            TempData["SuccessMessage"] = "Expense deleted successfully.";
-            _expensesService.DeleteExpenses(ExpenseId);
-            return RedirectToAction("Index");
-        }
-        #endregion
+        _expensesService = expensesService;
     }
+
+    public IActionResult Index()
+    {
+        var expenses = _expensesService.GetExpenseByUserId(UserId);
+        var categories = _expensesService.GetCategories();
+
+        // Retrieve user's currency preference
+        var currencySymbol = GetCurrencyPreference(UserId) ?? "₱"; // Default to PHP
+
+        var data = new ExpenseDataModel
+        {
+            ExpenseViewModel = expenses,
+            CategoryViewModel = categories
+        };
+
+        // Pass the currency symbol to the view
+        ViewBag.CurrencySymbol = currencySymbol;
+
+        return View(data);
+    }
+
+    #region Helper Methods
+    private string GetCurrencyPreference(string userId)
+    {
+        // Replace this with actual logic to retrieve the user's preference
+        // For example, from the database or session:
+        // return _userService.GetCurrencyPreferenceByUserId(userId);
+        return "₱"; // Default currency
+    }
+    #endregion
+
+    #region Get Methods
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var data = _expensesService.RetrieveExpenses(id);
+        return View(data);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var data = _expensesService.RetrieveExpenses(id);
+        return View(data);
+    }
+    #endregion
 }
